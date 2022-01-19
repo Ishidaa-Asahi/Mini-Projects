b6ef441f6c6c40b36a88518c1714e240082b5bc5
Sadhasivam <62686520+starksiv@users.noreply.github.com>
2022-01-19 19:25:32 +0530

    Added variables and methods

--- Arithmetic/app/src/main/java/com/example/arithmetic/MainActivity.java
+++ Arithmetic/app/src/main/java/com/example/arithmetic/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.arithmetic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText num1;
+    private EditText num2;
+    private Button add;
+    private Button sub;
+    private Button mul;
+    private Button div;
+    private TextView answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //transferring xml values to java variables
+        num1 = (EditText) findViewById(R.id.num1);
+        num2 = (EditText) findViewById(R.id.num2);
+        add = (Button) findViewById(R.id.addBtn);
+        sub = (Button) findViewById(R.id.subBtn);
+        mul = (Button) findViewById(R.id.mulBtn);
+        div = (Button) findViewById(R.id.divBtn);
+        answer = (TextView) findViewById(R.id.ans);
+
+        add.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //int number1 = ;
+            }
+        });
     }
 }
